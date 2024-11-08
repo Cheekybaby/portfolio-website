a042cb3e7b1e509237a8ac529bf91daf7f7b3fe5
Shashank Ranjan <shashankranjan07@gmail.com>
2024-11-08 23:22:10 +0530

    Changed the favicon and the title

--- src/app/page.js
+++ src/app/page.js
@@ -7,15 +7,21 @@ import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <body className="m-0">
-      <NavBar />
-      <main className="flex min-h-screen flex-col container mx-auto px-2 py-2">
-        <HeroSection />
-        <AboutMe />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </body>
+    <>
+      <head>
+        <title>Shashank Ranjan</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className="m-0">
+        <NavBar />
+        <main className="flex min-h-screen flex-col container mx-auto px-2 py-2">
+          <HeroSection />
+          <AboutMe />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </body>
+    </>
   );
 }
